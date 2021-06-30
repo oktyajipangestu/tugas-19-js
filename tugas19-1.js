0db413c2a44d74e7283967d01cf4fb25b7a8edc9
oktyajipangestu <86592837+oktyajipangestu@users.noreply.github.com>
2021-06-30 21:41:20 +0700

    Add files via upload

---
+++ tugas19-1.js
@@ -0,0 +1,3 @@
+let text = "Semangat...";
+
+console.log(text.repeat(5));
---
+++ tugas19-2.js
@@ -0,0 +1,9 @@
+console.log("*".repeat(5));
+console.log("*".repeat(25));
+console.log("*".repeat(45));
+console.log("*".repeat(65));
+console.log("*".repeat(85));
+console.log("*".repeat(65));
+console.log("*".repeat(45));
+console.log("*".repeat(25));
+console.log("*".repeat(5));
